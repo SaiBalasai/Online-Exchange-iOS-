607bd9ea401700724bd47094b11c808929790d27
Kumarchandu21 <kumarchandu218@gmail.com>
2024-09-12 23:56:33 -0500

    added some functiionalities for the project

---
+++ codes/Online Exchange/ProductCell.swift
@@ -0,0 +1,12 @@
+//
+//  ProductCell.swift
+//  Online Exchange
+//
+//  Created by Kumar Chandu on 9/12/24.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+}
---
+++ codes/Online Exchange/WishlistManager.swift
@@ -0,0 +1,8 @@
+//
+//  WishlistManager.swift
+//  Online Exchange
+//
+//  Created by Kumar Chandu on 9/12/24.
+//
+
+import Foundation
