83c5c816030d490c19ba30f528ed4e8c2632ea19
Kumarchandu21 <kumarchandu218@gmail.com>
2024-10-27 16:56:07 -0500

    Kumar Chandu, Sai Kishore: Added fedback UI and its working fucntionality

---
+++ OnlineExchange 3/OnlineExchange/FeedBackVC.swift
@@ -0,0 +1,29 @@
+//
+//  FeedBackVC.swift
+//  OnlineExchange
+//
+//  Created by Kumar Chandu on 10/21/24.
+//
+
+import UIKit
+
+class FeedBackVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
